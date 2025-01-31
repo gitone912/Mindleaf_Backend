57be41e1ec653c0253e374cf1a95ca2abd6dc843
gitone912 <akash.101@kvsroranchi.in>
2025-02-01 02:09:36 +0530

    d

--- app.js
+++ app.js
@@ -52,11 +52,11 @@ app.use((err, req, res, next) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-// app.listen(PORT, '0.0.0.0', () => {
-//   console.log(`Server running on http://0.0.0.0:${PORT}`);
-// });
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://0.0.0.0:${PORT}`);
+// });
 
 module.exports = app;
